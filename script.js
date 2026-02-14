a8b6e1d6ee8b18248bd816b5d304da7d53c318bb
jcmando268-alt <jc.mando.268@gmail.com>
2026-02-14 16:33:32 +0800

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+    function initFlipbook() {
+        const containerWidth = $('.book-container').width();
+        const containerHeight = containerWidth * 0.75; // maintain 4:3 ratio
+        $("#book").turn({
+            width: containerWidth,
+            height: containerHeight,
+            autoCenter: true,
+            gradients: true,
+            elevation: 50
+        });
+    }
+
     // Initialize flipbook
-    $("#book").turn({
-        width: 800,
-        height: 600,
-        autoCenter: true,
-        gradients: true,
-        elevation: 50,
-        when: {
-            turned: function(event, page, view) {
-                // optional: could trigger something on page flip
-            },
-            start: function(event) {
-                // optional: could trigger something on start
-            }
-        }
+    initFlipbook();
+
+    // Reinitialize on window resize
+    $(window).resize(function() {
+        $("#book").turn('destroy').remove(); // remove old book
+        $('.book-container').html('<div id="book">' + $('.book-container #book').html() + '</div>');
+        initFlipbook();
     });
 
     // Navigation Buttons
@@ -25,21 +30,15 @@ $(document).ready(function(){
         $("#book").turn('next');
     });
 
-    // Fade-in background music automatically on page load
+    // Background music
     var audio = document.getElementById('bg-music');
-    
-    // Try to play immediately (some browsers may block)
     audio.volume = 0;
     var playPromise = audio.play();
 
     if (playPromise !== undefined) {
         playPromise
-            .then(() => {
-                // Music started successfully
-                fadeInMusic(audio, 5000); // 5-second fade-in
-            })
+            .then(() => fadeInMusic(audio, 5000))
             .catch(() => {
-                // Browser blocked autoplay, fall back to first click
                 $(document).one('click', function(){
                     audio.play();
                     fadeInMusic(audio, 5000);
@@ -47,7 +46,6 @@ $(document).ready(function(){
             });
     }
 
-    // Function to fade in music
     function fadeInMusic(audioElement, duration) {
         var stepTime = 100;
         var step = stepTime / duration;
@@ -60,8 +58,3 @@ $(document).ready(function(){
         }, stepTime);
     }
 });
-
-
-
-
-
